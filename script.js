d2eb8c7e3415105d28d4f8d867a2ab59c90e635d
Kent Huntsman <kenthuntsman@yahoo.com>
2018-03-26 12:17:53 -0600

    Add responsive css
    Fix incorrect variable with the createCard variable
    add link to the reset.css
    add <meta>
    change css width and height to 50% and 100% respectivly on class .main-right

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-var cardTitle = document.querySelector('.main-left-input-title');
+var cardTitle = document.querySelector('.main-left-input-field');
 var cardList = document.querySelector('.main-right-card-list');
 var enterButton = document.querySelector('.main-left-button-enter');
-var cardURL = document.querySelector('.main-left-input-URL');
+var cardURL = document.querySelector('.main-left-inputURL');
 
 // enterButton.addEventListener('click', appendCard);
 enterButton.addEventListener('click', blankOnEntry);
@@ -11,36 +10,35 @@ cardList.addEventListener('click', markAsRead);
 
 
 function appendCard() {
-  var createCard = document.createElement('li');  
+ var createCard = document.createElement('li');  
 
-  createCard.innerHTML = `
-    <h3 class="main-right-card-title">${cardTitle.value}</h3>
-    <a class="main-right-card-link" href="">${cardURL.value}</a>
-    <button class= "main-right-card-button main-right-card-button-read">Read</button>
-    <button class= "main-right-card-button main-right-card-button-delete">Delete</button>`;
-  cardList.appendChild(createCard); 
+ createCard.innerHTML = `<h3 class="main-right-card-title">${cardTitle.value}</h3>
+   <a class="main-right-card-link" href="">${cardURL.value}</a>
+   <button class= "main-right-card-button main-right-card-button-read">Read</button>
+   <button class= "main-right-card-button main-right-card-button-delete">Delete</button>`;
+ cardList.appendChild(createCard); 
 }
 
 
 function deleteCard(event) {
-  if(event.target.matches('.main-right-card-button-delete')){
-    event.target.closest('li').remove();
-  }
+ if(event.target.matches('.main-right-card-button-delete')){
+   event.target.closest('li').remove();
+ }
 }
 
 function markAsRead() {
-  if (event.target && event.target.matches('.main-right-card-button-read')){
-    event.target.closest('li').classList.toggle('read');
-  }
+ if (event.target && event.target.matches('.main-right-card-button-read')){
+   event.target.closest('li').classList.toggle('read');
+ }
 
-  document.querySelector('.main-right-card-button-read').classList.toggle('main-right-card-button-read-clicked');
-  document.querySelector('.main-right-card-button-delete').classList.toggle('main-right-card-button-delete-read');
+ document.querySelector('.main-right-card-button-read').classList.toggle('main-right-card-button-read-clicked');
+ document.querySelector('.main-right-card-button-delete').classList.toggle('main-right-card-button-delete-read');
 }
- 
+
 function blankOnEntry() {
-  if (cardURL.value === '' || cardTitle.value === ''){
-    alert('Error empty field')
-  } else {
-    appendCard()
-  }
+ if (cardURL.value == '' || cardTitle.value == ''){
+   alert('Error empty field');
+ } else {
+   appendCard();
+ }
 }

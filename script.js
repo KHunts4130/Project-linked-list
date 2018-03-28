6df33ca46dc66d6388fae54c5efcb9d18b8f7454
Kent Huntsman <kenthuntsman@yahoo.com>
2018-03-28 17:27:59 -0600

    Add css and html
    fix title font size
    remove height of main left logo
    fix hover pn read and delete button
    add pointer on hover
    remove placeholder li cards
    add <main> element in html to fix alignment for right and left containers
    add overflow scroll to .main-right
    add <main> css properties
    add === to function blank entry
    add transperant property to read and delete buttons

--- script.js
+++ script.js
@@ -10,35 +10,53 @@ cardList.addEventListener('click', markAsRead);
 
 
 function appendCard() {
- var createCard = document.createElement('li');  
-
- createCard.innerHTML = `<h3 class="main-right-card-title">${cardTitle.value}</h3>
-   <a class="main-right-card-link" href="">${cardURL.value}</a>
-   <button class= "main-right-card-button main-right-card-button-read">Read</button>
-   <button class= "main-right-card-button main-right-card-button-delete">Delete</button>`;
- cardList.appendChild(createCard); 
+var createCard = document.createElement('li');  
+ allCounters();
+createCard.innerHTML = `<h3 class="main-right-card-title">${cardTitle.value}</h3>
+  <a class="main-right-card-link" href="">${cardURL.value}</a>
+  <button class= "main-right-card-button main-right-card-button-read">Read</button>
+  <button class= "main-right-card-button main-right-card-button-delete">Delete</button>`;
+cardList.appendChild(createCard); 
 }
 
 
 function deleteCard(event) {
- if(event.target.matches('.main-right-card-button-delete')){
-   event.target.closest('li').remove();
- }
+if(event.target.matches('.main-right-card-button-delete')){
+  event.target.closest('li').remove();
+}
 }
 
 function markAsRead() {
- if (event.target && event.target.matches('.main-right-card-button-read')){
-   event.target.closest('li').classList.toggle('read');
- }
+if (event.target && event.target.matches('.main-right-card-button-read')){
+  event.target.closest('li').classList.toggle('read');
+}
 
- document.querySelector('.main-right-card-button-read').classList.toggle('main-right-card-button-read-clicked');
- document.querySelector('.main-right-card-button-delete').classList.toggle('main-right-card-button-delete-read');
+document.querySelector('.main-right-card-button-read').classList.toggle('.main-right-card-button-read-clicked');
+document.querySelector('.main-right-card-button-delete').classList.toggle('.main-right-card-button-delete-read');
 }
 
 function blankOnEntry() {
- if (cardURL.value == '' || cardTitle.value == ''){
-   alert('Error empty field');
- } else {
-   appendCard();
- }
+if (cardURL.value == '' || cardTitle.value == ''){
+  alert('Error empty field');
+} else {
+  appendCard();
+}
+}
+
+function cardSum() {
+   return $('li').length;
+}
+
+function cardsRead() {
+   return $('li .read').length;
+}
+
+function cardsUnread() {
+   return $('li').not('.read').length;
+}
+
+function allCounters() {
+   $('.cardSum').text('Number of Links: ' + cardSum());
+   $('.cardsRead').text('Number of Links Read: ' + cardsRead());
+   $('.cardsUnread').text('Number Unread: ' + cardsUnread());    
 }
